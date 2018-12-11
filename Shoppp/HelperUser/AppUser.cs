ddd4d50cbfb5ec1c60b195c6ba636ac37e24372e
Ajdin Ljubunčić <ajdin.ljubuncic@edu.fit.ba>
2018-12-11 17:59:27 +0000

    Updated AppUser.cs

--- Shoppp/HelperUser/AppUser.cs
+++ Shoppp/HelperUser/AppUser.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
-namespace OnlineShopping.HelperUser
+using Shoppp.Models;
+
+namespace Shoppp.HelperUser
 {
     public class AppUser : IdentityUser
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public Cart Korpa { get; set; }
+        public Favorites Omiljeni { get; set; }
+        public int Rate { get; set; }
     }
 }
